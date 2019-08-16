9412ce09eb5d4cf5fff5d38449e35c0072eca8f6
SCS MacBook Pro <scsmacbookpro@SCSs-MacBook-Pro.local>
2019-08-16 15:09:10 +0800

    Initial commit

---
+++ precache-manifest.dce5521570a64ba3befc40659e59ec0a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee6ec13c01e75fcfb0b576ba99a08aff",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c022b5915e7541178e82",
+    "url": "/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "7b3f1e6aac3b889dacaa",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "c022b5915e7541178e82",
+    "url": "/static/js/2.9e4394c6.chunk.js"
+  },
+  {
+    "revision": "7b3f1e6aac3b889dacaa",
+    "url": "/static/js/main.4487be6d.chunk.js"
+  },
+  {
+    "revision": "2b7c090f79fcae89dcb4",
+    "url": "/static/js/runtime~main.c0400a62.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
